4b22fa8f18acea8e048008607ad1b7d9a2798e11
julienjeanjacquot@gmail.com <julienjeanjacquot@gmail.com>
2021-06-10 10:42:48 +0200

    elev1

---
+++ elevator.c
@@ -0,0 +1,22 @@
+#include "elevator.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+Elevator *create_elevator(int capacity, int currentFloor, PersonList *persons)
+{
+    Elevator *elevator = (Elevator *)malloc(sizeof(Elevator));
+    elevator->capacity = capacity;
+    elevator->currentFloor = currentFloor;
+    elevator->targetFloor = NULL;
+    elevator->persons;
+}
+
+Building *create_building(int nbFloor, Elevator *elevator, PersonList **waitingLists)
+{
+    Building *building = (Building *)malloc(sizeof(Building));
+    building->nbFloor = nbFloor;
+    building->elevator = elevator;
+    building->waitingLists = waitingLists;
+}
+
+PersonList *exitElevator(Elevator *e);
