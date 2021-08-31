e92a2dd98cd20572773b8820e13bbb2bce24cac5
julienjeanjacquot@gmail.com <julienjeanjacquot@gmail.com>
2021-08-31 15:30:07 +0200

    add

--- elevator.c
+++ elevator.c
@@ -7,9 +7,8 @@ Elevator *create_elevator(int capacity, int currentFloor, PersonList *persons)
     Elevator *elevator = (Elevator *)malloc(sizeof(Elevator));
     elevator->capacity = capacity;
     elevator->currentFloor = currentFloor;
-    elevator->targetFloor = NULL;
+    elevator->targetFloor = 0;
     elevator->persons = persons;
-
     return elevator;
 }
 
@@ -20,7 +19,52 @@ Building *create_building(int nbFloor, Elevator *elevator, PersonList **waitingL
     building->elevator = elevator;
     building->waitingLists = waitingLists;
 
-    return elevator;
+    return building;
 }
 
-PersonList *exitElevator(Elevator *e);
+PersonList *exitElevator(Elevator *e)
+{
+    PersonList *List_Exit = malloc(e->capacity * sizeof(PersonList *));
+    PersonList *curentList = e->persons;
+
+    while (curentList != NULL)
+    {
+
+        if (curentList->person->dest == e->currentFloor)
+        {
+            insert(curentList->person, List_Exit);
+            curentList->next = curentList->next->next;
+            curentList->person = curentList->next->person;
+        }
+        curentList = curentList->next;
+    }
+    return List_Exit;
+}
+
+PersonList *enterElevator(Elevator *e, PersonList *list)
+{
+    int Places = 0;
+    PersonList *List_Count = malloc(e->capacity * sizeof(PersonList *));
+    List_Count = e->persons;
+    while (List_Count->person != NULL)
+    {
+        Places++;
+    }
+
+    for (int i = 0; i < Places; i++)
+    {
+        if (list->person != NULL)
+        {
+            e->persons = insert(list->person, e->persons);
+            list = list->next;
+        }
+    }
+    return list;
+}
+
+void stepElevator(Building *b)
+{
+    b->waitingLists[b->elevator->currentFloor] = enterElevator(b->elevator, b->waitingLists[b->elevator->currentFloor]);
+    b->elevator->currentFloor = b->elevator->targetFloor;
+    exitElevator(b->elevator);
+};
--- person.c
+++ person.c
@@ -12,7 +12,7 @@ Person *createPerson(int src, int dest)
 PersonList *insert(Person *p, PersonList *list)
 {
     PersonList *new_list = (PersonList *)malloc(sizeof(PersonList));
-    new_list->Person = p;
+    new_list->person = p;
     new_list->next = list;
     return new_list;
 }
